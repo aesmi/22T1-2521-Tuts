6161fb3009be44b6ca6c1c222f875b98d793ab2c
James Davidson <jedavidson2000@gmail.com>
2021-06-09 12:21:46 +1000

    Add annotated length example

--- cs2521/21t2/w09b/wk2/length.c
+++ cs2521/21t2/w09b/wk2/length.c
@@ -13,6 +13,9 @@ int stringLengthRec(char *s) {
     if (*s == '\0') {
         return 0;
     } else {
+        // The "s + 1" bit gives us the rest of the string minus the
+        // first character using some pointer arithmetic.
+        // It's equivalent to &s[1], if you'd prefer that way.
         return 1 + stringLengthRec(s + 1);
     }
 }
