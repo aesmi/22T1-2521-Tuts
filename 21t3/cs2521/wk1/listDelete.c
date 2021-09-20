8304dfcc425f275206fa555763159a5d5e26437c
James Davidson <jedavidson2000@gmail.com>
2021-09-20 21:14:49 +1000

    Add solutions to practical exercises for week 1

--- 21t3/cs2521/wk1/listDelete.c
+++ 21t3/cs2521/wk1/listDelete.c
@@ -8,7 +8,25 @@
  * Deletes the first occurrence of a value from a linked list.
  */
 void listDelete(ConList cl, int value) {
-    // TODO: Complete this function!
+    // If we are deleting a node after the head, we'll need to keep track
+    // of the node that came before it to properly re-link it.
+    Node *prev = NULL;
+    Node *curr = cl->head;
+    while (curr != NULL && curr->value != value) {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    // At this point, we know that we're either at the end of the list
+    // or we're at the first node that contains the value we're after.
+    if (curr != NULL) {
+        if (curr == cl->head) {
+            cl->head = curr->next;
+        } else {
+            prev->next = curr->next;
+        }
+        free(curr);
+    }
 }
 
 
--- 21t3/cs2521/wk1/listSum.c
+++ 21t3/cs2521/wk1/listSum.c
@@ -7,15 +7,34 @@
 /**
  * Returns the sum of all elements in a linked list.
  */
-int listSum(List l) {
-    // TODO: Complete this function!
-    return 0;
+int listSumWhile(List l) {
+    int sum = 0;
+    Node *curr = l;
+    while (curr != NULL) {
+        sum += curr->value;
+        curr = curr->next;
+    }
+    return sum;
+}
+
+/**
+ * The same function, but using a for loop instead to traverse the list.
+ * Generally speaking, while loops are a bit nicer for linked list stuff,
+ * as the for loops can quickly get out of hand (i.e. line length).
+ */
+int listSumFor(List l) {
+    int sum = 0;
+    for (Node *curr = l; curr != NULL; curr = curr->next) {
+        sum += curr->value;
+    }
+    return sum;
 }
 
 
 int main(int argc, char **argv) {
     List l = readList(argc, argv);
-    printf("List sum: %d\n", listSum(l));
+    printf("List sum (while): %d\n", listSumWhile(l));
+    printf("List sum (for): %d\n", listSumFor(l));
     freeList(l);
 
     return EXIT_SUCCESS;
