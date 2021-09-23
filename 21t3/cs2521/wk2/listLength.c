f0d6a4a4bc1d72d48e9be71fd2864b0111a376df
James Davidson <jedavidson2000@gmail.com>
2021-09-23 13:39:41 +1000

    Add extra code in listLength starter

--- 21t3/cs2521/wk2/listLength.c
+++ 21t3/cs2521/wk2/listLength.c
@@ -11,13 +11,29 @@ int listLength(List list) {
     return -1;
 }
 
+/**
+ * The same thing, but using recursion!
+ */
+int listLengthRec(List list) {
+    // TODO: Complete this function!
+    return -1;
+}
+
 
 int main(int argc, char **argv) {
     List list = readList(argc, argv);
     printf("Input list: ");
     showList(list);
 
-    printf("The list has length %d\n", listLength(list));
+    printf(
+        "The iterative function says the list has length %d\n",
+        listLength(list)
+    );
+
+    printf(
+        "The recursive function says the list has length %d\n",
+        listLengthRec(list)
+    );
 
     freeList(list);
     return 0;
