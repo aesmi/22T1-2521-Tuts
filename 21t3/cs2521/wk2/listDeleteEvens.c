2441d0ca226f1d75a6fa7f651c50e2ef2e4df663
James Davidson <jedavidson2000@gmail.com>
2021-09-29 23:28:08 +1000

    Add solutions to practical exercises for week 2

--- 21t3/cs2521/wk2/listDeleteEvens.c
+++ 21t3/cs2521/wk2/listDeleteEvens.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
 #include "List.h"
 
@@ -9,8 +9,30 @@
  * to what remains of the original list afterwards.
  */
 List listDeleteEvens(List list) {
-    // TODO: Complete this function!
-    return NULL;
+    // Base case: the empty list doesn't contain any evens.
+    if (list == NULL) {
+        return list;
+    }
+
+    // We have access to the rest of the list that comes after the head
+    // of the current list we have (i.e. the "list" in the argument).
+    List rest = list->next;
+
+    // Recursive case: the head of the list is a node with an even number.
+    // This node should be deleted, and then afterwards we can just return
+    // what remains of the rest of the list after also removing all of the
+    // even numbers from that.
+    if (list->data % 2 == 0) {
+        free(list);
+        return listDeleteEvens(rest);
+
+    // Recursive case: the head of the list is a node with an odd number.
+    // We handle this similarly to the previous case, except that we don't
+    // delete the current node. What comes after might've changed though!
+    } else {
+        list->next = listDeleteEvens(list->next);
+        return list;
+    }
 }
 
 
--- 21t3/cs2521/wk2/listLength.c
+++ 21t3/cs2521/wk2/listLength.c
@@ -7,16 +7,29 @@
  * Computes the length of a linked list.
  */
 int listLength(List list) {
-    // TODO: Complete this function!
-    return -1;
+    int length = 0;
+    Node *curr = list;
+    while (curr != NULL) {
+        length++;
+        curr = curr->next;
+    }
+    return length;
 }
 
 /**
  * The same thing, but using recursion!
  */
 int listLengthRec(List list) {
-    // TODO: Complete this function!
-    return -1;
+    // Base case: the empty list has length 0.
+    if (list == NULL) {
+        return 0;
+    }
+
+    // Recursive case: we have a node followed by the rest of the list.
+    // If we can find out the length of the rest, we can say that the
+    // length of the list we're currently looking at is just 1 greater.
+    List rest = list->next;
+    return 1 + listLengthRec(rest);
 }
 
 
